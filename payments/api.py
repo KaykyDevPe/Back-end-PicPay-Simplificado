477d5580d8566d614603a7f15ee0729699344ca7
KaykyDevPe <kaykyr3123daniel@gmail.com>
2024-08-17 15:28:31 -0300

    Finalizacao do projeto

---
+++ payments/api.py
@@ -0,0 +1,46 @@
+from ninja import Router
+from .schemas import TransactionSchema
+from django.shortcuts import get_object_or_404
+from users.models import User
+from rolepermissions.checkers import has_permission
+from django.db import transaction as django_transaction
+import requests
+from django.conf import settings
+from .models import Transactions
+from django_q.tasks import async_task
+from .tasks import send_notification
+
+payments_router = Router()
+
+@payments_router.post('/', response={200: dict, 400: dict, 403: dict})
+def transaction(request, transaction: TransactionSchema):
+    payer = get_object_or_404(User, id=transaction.payer)
+    payee = get_object_or_404(User, id=transaction.payee)
+
+    if payer.amount < transaction.amount:
+        return 400, {'error': 'Saldo insuficiente'}
+    
+    if not has_permission(payer, 'make_transfer'):
+        return 403, {'error': 'Você não possui permissão para realizar transferências'}
+    
+    if not has_permission(payee, 'receive_transfer'):
+        return 403, {'error': 'O usuário não pode receber transferências'}
+    
+    with django_transaction.atomic():
+        payer.pay(transaction.amount)
+        payee.receive(transaction.amount)
+
+        transct = Transactions(
+            amount = transaction.amount,
+            payer_id = transaction.payer,
+            payee_id = transaction.payee
+        )
+        payer.save()
+        payee.save()
+        transct.save()
+
+        response = requests.get(settings.AUTHORIZE_TRANSFER_ENDPOINT).json()
+        if response.get('status') != "authorized":
+            raise Exception()
+    async_task(send_notification, payer.first_name, payee.first_name, transaction.amount)
+    return 200, {'transaction_id': 1}
--- users/api.py
+++ users/api.py
@@ -1,8 +1,27 @@
 from ninja import Router
-from .schemas import UserSchema
+from .schemas import UserSchema, TypeUserSchema
+from .models import User
+from django.contrib.auth.hashers import make_password
+from django.core.exceptions import ValidationError
+from rolepermissions.roles import assign_role
+
 users_router = Router()
 
-@users_router.post('/', response={200: dict})
-def create_user(request, user: UserSchema):
-    print(user.dict())
-    return {'ok':'ok'}
+@users_router.post('/', response={200: dict, 400: dict, 500: dict,})
+def create_user(request, type_user_schema: TypeUserSchema):
+    print(type_user_schema.user.dict())
+    print(type_user_schema.type_user.dict())
+
+    user = User(**type_user_schema.user.dict())
+    user.password=make_password(type_user_schema.user.password)
+    try:
+        user.full_clean()
+        user.save()
+        assign_role(user, type_user_schema.type_user.type)
+    except ValidationError as e:
+        print(e.message_dict)
+        return 400, {'errors': e.message_dict}
+    except Exception as e:
+        return 500, {'errors': 'Erro interno do servidor, contate um adm'}
+
+    return {'user_id': user.id}
